924857f550a5cfcca5c2ae61f7633504cb68bc37
LuAbelt <l.abelt@luabelt.de>
2023-02-16 14:12:47 +0000

    * Finished initial version of liquid type checker

--- aeon/typing/entailment.py
+++ aeon/typing/entailment.py
@@ -1,18 +1,9 @@
 from __future__ import annotations
 
-from typing import Tuple
-
-from aeon.core.liquid import LiquidLiteralBool
-from aeon.core.liquid import LiquidTerm
 from aeon.core.liquid import LiquidVar
-from aeon.core.substitutions import substitution
 from aeon.core.substitutions import substitution_in_liquid
 from aeon.core.types import AbstractionType
-from aeon.core.types import BaseType
 from aeon.core.types import extract_parts
-from aeon.core.types import RefinedType
-from aeon.core.types import Type
-from aeon.core.types import TypeVar
 from aeon.typing.context import EmptyContext
 from aeon.typing.context import TypeBinder
 from aeon.typing.context import TypingContext
@@ -20,8 +11,6 @@ from aeon.typing.context import VariableBinder
 from aeon.verification.horn import solve
 from aeon.verification.vcs import Constraint
 from aeon.verification.vcs import Implication
-
-# from aeon.verification.smt import smt_valid
 
 
 def entailment(ctx: TypingContext, c: Constraint):
